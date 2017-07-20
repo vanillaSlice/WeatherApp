65915a7640592fed5842f117d1866fb351051ce2
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-07-20 19:06:47 +0100

    Updated to use github pages

--- README.md
+++ README.md
@@ -3,9 +3,4 @@ Intermediate Front End Development Project for FreeCodeCamp.
 
 The [simpleWeather jQuery plugin](http://simpleweatherjs.com/) is used to pull in weather information for this project.
 
-This page is intended to be functionally similar to [this example page](https://codepen.io/freeCodeCamp/full/bELRjV).
-
-A codepen of my page can be found [here](https://codepen.io/vanillaSlice/full/OmYLwL/).
-
-## Screenshot
-![screenshot](./screenshot.png)
+A working version of this page can be found [here](https://vanillaslice.github.io/WeatherApp/).
--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 $(document).ready(function () {
-  
+
   "use strict";
-  
+
   var geolocation = navigator.geolocation,
     container = $("#container"),
     defaultUnit = $("input[name='unit']:checked").val(),
     unit = defaultUnit,
     weather,
     daysForecast = 5;
-  
+
   if (!geolocation) {
     handleUnsupportedGeolocation();
   } else {
     handleSupportedGeolocation();
   }
-  
+
   function handleUnsupportedGeolocation() {
     container.html("Geolocation is not supported by this browser");
   }
-  
+
   function handleSupportedGeolocation() {
     geolocation.getCurrentPosition(handleGetCurrentPositionSuccess,
-                                   handleGetCurrentPositionError,
-                                  {enableHighAccuracy: true});
+      handleGetCurrentPositionError, {
+        enableHighAccuracy: true
+      });
   }
-  
+
   function handleGetCurrentPositionSuccess(position) {
     var coords = position.coords;
     $.simpleWeather({
@@ -34,13 +35,13 @@ $(document).ready(function () {
       error: handleSimpleWeatherError
     });
   }
-  
+
   function handleSimpleWeatherSuccess(data) {
     weather = data;
     // change current day name
     weather.forecast[0].day = "Today";
     $("#temperature-container").css("background-image",
-                                    "url(" + weather.image + ")");
+      "url(" + weather.image + ")");
     setCurrentTemperature();
     $("#city").html(weather.city);
     $("#country").html(weather.country);
@@ -49,34 +50,36 @@ $(document).ready(function () {
     $("#loader").addClass("hidden");
     $("#weather-container").addClass("fade-in").removeClass("hidden");
   }
-  
+
   function setCurrentTemperature() {
     var currentTemperature = (unit === defaultUnit) ? weather.temp :
-                                                      weather.alt.temp;
+      weather.alt.temp;
     $("#temperature").html(currentTemperature + "&deg;");
   }
-  
+
   function setForecast() {
     for (var i = 0; i <= daysForecast; i++) {
       var forecast = (unit === defaultUnit) ? weather.forecast[i] :
-                                              weather.forecast[i].alt;
+        weather.forecast[i].alt;
       var dayId = "#day-" + i;
       $(dayId + " .day").html(forecast.day);
-      $(dayId + " .forecast-thumbnail").attr({title: forecast.text,
-                                              src: forecast.thumbnail});
+      $(dayId + " .forecast-thumbnail").attr({
+        title: forecast.text,
+        src: forecast.thumbnail
+      });
       $(dayId + " .high").html(forecast.high + "&deg;");
       $(dayId + " .low").html(forecast.low + "&deg;");
     }
   }
-  
+
   function handleSimpleWeatherError(error) {
     container.html(error);
   }
-  
+
   function handleGetCurrentPositionError(error) {
     container.html("Could not get current position: " + error.message);
   }
-  
+
   $("input[name='unit']").click(function () {
     // only update temperatures if unit is different
     if (unit !== this.value) {
@@ -85,5 +88,5 @@ $(document).ready(function () {
       setForecast();
     }
   });
-  
+
 });
