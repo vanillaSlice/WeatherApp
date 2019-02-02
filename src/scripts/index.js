e41766b76ca97482f687f59a02ee04cdefc2d84e
Mike Lowe <mikelowedev@gmail.com>
2019-02-02 17:55:50 +0000

    Switch to apixu api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,49 @@
+/*
+ * Constants
+ */
+
+const apiKey = '1848f6048f074277990152812190202';
+const baseUrl = 'http://api.apixu.com/v1/forecast.json';
 const { geolocation } = navigator;
 const defaultUnit = 'c';
-const daysToForecast = 5;
+const daysToForecast = 6;
+const dayNames = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+];
+
+/*
+ * Variables
+ */
+
 let currentUnit = defaultUnit;
 let currentWeather;
 
 /*
- * DOM elements
+ * DOM Elements
  */
 
-const bodyElement = $(document.body);
-const loaderElement = $('.loader');
-const weatherPanelElement = $('.weather-panel');
-const temperatureContainerElement = $('.temperature-container');
-const temperatureElement = $('.temperature-container__temperature');
-const unitElements = $('.temperature-container__unit');
-const cityElement = $('.weather-panel__city');
-const countryElement = $('.weather-panel__country');
-const currentConditionElement = $('.weather-panel__current-condition');
+const bodyElement = $('.js-body');
+const loaderElement = $('.js-loader');
+const weatherPanelElement = $('.js-weather-panel');
+const temperatureContainerElement = $('.js-temperature-container');
+const temperatureElement = $('.js-temperature');
+const unitElements = $('.js-unit');
+const cityElement = $('.js-city');
+const countryElement = $('.js-country');
+const currentConditionElement = $('.js-current-condition');
+
+/*
+ * Functions
+ */
 
 function displayErrorMessage(message) {
-  bodyElement.html(`<p class="error-message">${message}</p>`);
+  bodyElement.html(`<p class="error-message js-error-message">${message}</p>`);
 }
 
 function handleUnsupportedGeolocation() {
@@ -27,65 +51,59 @@ function handleUnsupportedGeolocation() {
 }
 
 function setCurrentTemperature() {
-  const currentTemperature = (currentUnit === defaultUnit) ? currentWeather.temp
-    : currentWeather.alt.temp;
+  const currentTemperature = (currentUnit === 'c') ? currentWeather.current.temp_c : currentWeather.current.temp_f;
   temperatureElement.html(`${currentTemperature}&deg;`);
 }
 
 function setForecast() {
-  for (let i = 0; i <= daysToForecast; i += 1) {
-    const forecast = (currentUnit === defaultUnit) ? currentWeather.forecast[i]
-      : currentWeather.forecast[i].alt;
-    const dayId = `#day-${i}`;
-    $(`${dayId} .forecast-table__day`).html(forecast.day);
-    $(`${dayId} .forecast-table__thumbnail`).attr({
-      title: forecast.text,
-      src: forecast.thumbnail,
+  for (let i = 0; i < daysToForecast; i += 1) {
+    const forecast = currentWeather.forecast.forecastday[i];
+    const dayName = dayNames[new Date(forecast.date).getDay()];
+    const high = (currentUnit === 'c') ? forecast.day.maxtemp_c : forecast.day.maxtemp_f;
+    const low = (currentUnit === 'c') ? forecast.day.mintemp_c : forecast.day.mintemp_f;
+    $(`.js-day-${i} .js-day`).html(dayName);
+    $(`.js-day-${i} .js-thumbnail`).attr({
+      title: forecast.day.condition.text,
+      src: forecast.day.condition.icon,
     });
-    $(`${dayId} .forecast-table__high`).html(`${forecast.high}&deg;`);
-    $(`${dayId} .forecast-table__low`).html(`${forecast.low}&deg;`);
+    $(`.js-day-${i} .js-high`).html(`${high}&deg;`);
+    $(`.js-day-${i} .js-low`).html(`${low}&deg;`);
   }
 }
 
-function handleSimpleWeatherSuccess(data) {
-  currentWeather = data;
+function handleGetWeatherSuccess(res) {
+  currentWeather = res;
 
-  // change current day name
-  currentWeather.forecast[0].day = 'Today';
-
-  temperatureContainerElement.css('background-image', `url(${currentWeather.image})`);
+  temperatureContainerElement.css('background-image', `url(${res.current.condition.icon})`);
 
   setCurrentTemperature();
 
-  cityElement.html(currentWeather.city);
-  countryElement.html(currentWeather.country);
-  currentConditionElement.html(currentWeather.currently);
+  cityElement.html(res.location.name);
+  countryElement.html(res.location.country);
+  currentConditionElement.html(res.current.condition.text);
 
   setForecast();
 
-  // hide loader
   loaderElement.addClass('hidden');
 
-  // show weather panel
   weatherPanelElement.addClass('fade-in').removeClass('hidden');
 }
 
-function handleSimpleWeatherError(error) {
-  displayErrorMessage(error);
+function handleGetWeatherError(err) {
+  displayErrorMessage(err.responseJSON.error.message);
 }
 
 function handleGetCurrentPositionSuccess(position) {
-  const { coords } = position.coords;
-  $.simpleWeather({
-    location: `${coords.latitude},${coords.longitude}`,
-    unit: currentUnit,
-    success: handleSimpleWeatherSuccess,
-    error: handleSimpleWeatherError,
-  });
+  const { coords } = position;
+  const url = `${baseUrl}?key=${apiKey}&q=${coords.latitude},${coords.longitude}&days=${daysToForecast}`;
+
+  $.get(url)
+    .done(handleGetWeatherSuccess)
+    .fail(handleGetWeatherError);
 }
 
-function handleGetCurrentPositionError(error) {
-  displayErrorMessage(`Could not get current position: ${error.message}`);
+function handleGetCurrentPositionError(err) {
+  displayErrorMessage(`Could not get current position: ${err.message}`);
 }
 
 function handleSupportedGeolocation() {
@@ -96,19 +114,20 @@ function handleSupportedGeolocation() {
   );
 }
 
+function handleUnitChange(e) {
+  currentUnit = e.target.value;
+  setCurrentTemperature();
+  setForecast();
+}
+
+/*
+ * Initialise
+ */
+
 if (!geolocation) {
   handleUnsupportedGeolocation();
 } else {
   handleSupportedGeolocation();
 }
 
-unitElements.click(() => {
-  // only update temperatures if unit is different
-  if (currentUnit === this.value) {
-    return;
-  }
-
-  currentUnit = this.value;
-  setCurrentTemperature();
-  setForecast();
-});
+unitElements.click(handleUnitChange);
